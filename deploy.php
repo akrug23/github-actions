9230ca3e28e50d56c7c019d7a6d52e71b844f63a
Aaron Krug <aaron@baldmanweb.com>
2023-12-27 17:09:14 -0500

    update

--- deploy.php
+++ deploy.php
@@ -2,49 +2,21 @@
 
 namespace Deployer;
 
-// Include the Laravel & rsync recipes
 require 'recipe/laravel.php';
-require 'recipe/rsync.php';
 
-set('application', 'dep-demo');
-set('ssh_multiplexing', true); // Speed up deployment
+// Config
 
-set('rsync_src', function () {
-    return __DIR__; // If your project isn't in the root, you'll need to change this.
-});
+set('repository', 'https://github.com/akrug23/github-actions.git');
 
-// Configuring the rsync exclusions.
-// You'll want to exclude anything that you don't want on the production server.
-add('rsync', [
-    'exclude' => [
-        '.git',
-        '/.env',
-        '/storage/',
-        '/vendor/',
-        '/node_modules/',
-        '.github',
-        'deploy.php',
-    ],
-]);
-
-// Set up a deployer task to copy secrets to the server.
-// Grabs the dotenv file from the github secret
-task('deploy:secrets', function () {
-    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
-    upload('.env', get('deploy_path') . '/shared');
-});
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
 
 // Hosts
 
-host('actions.baldmanweb.com') // Name of the server
-    ->hostname('actions.baldmanweb.com') // Hostname or IP address
-    ->stage('production') // Deployment stage (production, staging, etc)
-    ->user('akrug23') // SSH user
-    ->set('deploy_path', '/home2/akrug23/actions.baldmanweb.com'); // Deploy path
-
-// host('http://actions.baldmanweb.com/')
-//     ->set('remote_user', 'deployer')
-//     ->set('deploy_path', '~/github-actions');
+host('actions.baldmanweb.com')
+    ->set('remote_user', 'akrug23')
+    ->set('deploy_path', '~/github-actions');
 
 // Hooks
 
